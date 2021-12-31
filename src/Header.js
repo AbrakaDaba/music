fe79c93847ad930318add1309192a6a8c43f498d
David <abrakadaba@gmail.com>
2021-12-31 12:33:19 +0100

    Adding objects to json data file

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>David's Top 10 Favorite</h1>
+        </header>
+     );
+}
+
+export default Header
---
+++ src/data.js
@@ -0,0 +1,81 @@
+    export const data = [
+        {
+            id:1,
+            song: "Turn The Page",
+            artist: "Bob Seger",
+            original:"Bob Seger",
+            src:"https://www.youtube.com/embed/GONmFCkCGCc"
+        },
+        {
+            id:2,
+            song: "Turn The Page",
+            artist: "Metallica",
+            original:"Bob Seger",
+            src:"https://www.youtube.com/embed/z5f-UdtOsXE"
+        },
+        {
+            id:3,
+            song: "ads",
+            artist: "asd",
+            original:"asdasd",
+            src:"https://www.youtube.com/embed/kTqwy6ay1HQ"
+        },
+        {
+            id:4,
+            song: "ads",
+            artist: "asd",
+            original:"asdasd",
+            src:"https://www.youtube.com/embed/23dW3H9yFSU"
+        },
+        {
+            id:5,
+            song: "ads",
+            artist: "asd",
+            original:"asdasd",
+            src:"https://www.youtube.com/embed/XYLOAay_2MQ"
+        },
+        {
+            id:6,
+            song: "ads",
+            artist: "asd",
+            original:"asdasd",
+            src:"https://www.youtube.com/embed/Vnm1yka12J8"
+        },
+        {
+            id:7,
+            song: "ads",
+            artist: "azxcsd",
+            original:"asdasd",
+            src:"https://www.youtube.com/embed/n8vFZNXV4i8"
+        },
+        {
+            id:8,
+            song: "azxczxczcxds",
+            artist: "asd",
+            original:"asdasd",
+            src:"https://www.youtube.com/embed/Eh2eDWVYiT8"
+        },
+        {
+            id:9,
+            song: "azxcds",
+            artist: "asd",
+            original:"asdasd",
+            src:"https://www.youtube.com/embed/MlMVagn-ZJw"
+        },
+        {
+            id:10,
+            song: "The Irish Rover",
+            artist: "The Pogues & The Dubliners",
+            original:"Joseph Crofts",
+            src:"https://www.youtube.com/embed/cZXnJ4UYh40"
+        },
+        {
+            id:11,
+            song: "You Shook Me All Night Long (Official Video)",
+            artist: "AC/DC",
+            original:"AC/DC",
+            src:"https://www.youtube.com/embed/Lo2qQmj0_h4"
+        },
+    ]
+  
+ 
